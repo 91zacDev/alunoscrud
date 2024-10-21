3d12433cefd93619fb0286f7dc796cb1617b92b6
91zacDev <zaq.c12@gmail.com>
2024-10-21 13:42:12 -0300

    Added AlunoRepository

--- src/main/java/com/escola/alunoscrud/repository/AlunoRepository.java
+++ src/main/java/com/escola/alunoscrud/repository/AlunoRepository.java
@@ -1,8 +1,9 @@
 package com.escola.alunoscrud.repository;
 
-import com.escola.alunoscrud.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import com.escola.alunoscrud.model.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-
+    List<Aluno> findByTurmaId(Long turmaId); // Exemplo de consulta customizada
 }
